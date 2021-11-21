be551dafe9d694c2cdeacf2463a2fc85fad52103
unknown <jacquesetheron@gmail.com>
2021-11-22 12:32:22 +1300

    Hope

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,session
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def home():
     return render_template('index.html')
 
+
+
+@app.route("/second", methods=['GET','POST'])
+def second():
+    if request.method == "POST":
+        if request.form.get("button_a"):
+            print("Find Pressed!!")
+            session["a"] = 2
+            if "a" in session:
+                a = session["a"]
+            return render_template('insurance.html', a = a)
+
+        if request.form.get("Yes1"):
+            print("Yes Pressed")
+        if request.form.get("No1"):
+            print("No Pressed")
+        if request.form.get("Man"):
+            print("Man Pressed")
+    
+    if "a" in session:
+        a = session["a"]
+        return render_template('insurance.html',a = a)
+    else:
+        return render_template('insurance.html')
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
