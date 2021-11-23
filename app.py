3e8ca35847c8bc6a1aecc681804a039553cd313d
unknown <jacquesetheron@gmail.com>
2021-11-23 23:49:24 +1300

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,session,jsonify
 
 app = Flask(__name__)
-app.secret_key = "hello"
+app.secret_key = "hellos"
 
 @app.route("/", methods=['GET','POST'])
 def home():
