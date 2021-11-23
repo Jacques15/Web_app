ab55077a48f50af0a388d2f5a1e3539a6c6499a0
unknown <jacquesetheron@gmail.com>
2021-11-23 23:01:08 +1300

    hope

--- app.py
+++ app.py
@@ -11,19 +11,43 @@ def home():
 def second():
     text = request.form.get("text")
     print(text)
+    session["text"] = text
     return render_template('insurance.html')
 
 @app.route("/last", methods=['GET','POST'])
 def last():
     print("In Last")
-    return render_template('page3.html')
+    if (session["text"] == "Check1"):
+        plan = "Comprehensive"
+        price = "$30.50 per fortnight"
+    if (session["text"] == "Check2"):
+        plan = "3rd Party Fire & Theft"
+        price = "$22.50 per fortnight"
+    if (session["text"] == "Check3"):
+        plan = "3rd Party"
+        price = "$10.00 per fortnight"
+    
+    return render_template('page3.html',plan = plan, price = price)
     
 if __name__ == '__main__':
     app.run()
 
 @app.route("/third", methods=['GET','POST'])
 def third():
+    text = request.form.get("text")
+    print(text)
+    session["text"] = text
     return render_template('Test.html')
+
+@app.route('/process', methods = ['POST','GET'])
+def process():
+    name = request.form.get("name")
+    surname = request.form.get("surname")
+
+    print(name)
+    print(surname)
+
+    return jsonify({'name':"Jacques", 'surname':"Theron"})
 
 
 
